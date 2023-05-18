7e6c0abf6858e0bb4cda46e7bab8a4ff3bb641a3
alanlipschutz <alanelipschutz@gmail.com>
2023-05-18 17:49:00 +0300

    feat: error fetching username. displayed message in front

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,40 @@
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/dist/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
+interface IUser {
+  id: string;
+  username: string;
+  profilePicture: string;
+}
+
+const filterUserForClient = (user: User) => {
+  const userInfo: IUser = {
+    id: user.id,
+    username: user.username ?? "user",
+    profilePicture: user.profileImageUrl ?? "",
+  };
+  return userInfo;
+};
+
 export const postRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+    });
+
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+      })
+    ).map(filterUserForClient);
+    console.log(users);
+
+    return posts.map((post) => ({
+      post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
