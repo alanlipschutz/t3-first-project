151dbc13fd5f6abb87c8636d286926013e40a5b1
alanlipschutz <alanelipschutz@gmail.com>
2023-05-21 17:33:01 +0300

    feat: if not username, replaced it with first name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
 
 const CreatePostWizard = () => {
   const { user } = useUser();
